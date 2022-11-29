103cad38869497777ac67c1f4292fb4cdcdde5e2
三吉優輝 <miyoshiyuki@mba.local>
2022-11-29 23:45:23 +0900

    add aws amplify

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { Amplify } from "aws-amplify";
+import { withAuthenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
 
-function App() {
+import awsExports from "./aws-exports";
+Amplify.configure(awsExports);
+
+function App({ signOut, user }) {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello React!!</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {user ? (
+          <>
+            <h3>私は権限を持っています: {user.username}</h3>
+            <button onClick={signOut}>サインアウト</button>
+          </>
+        ) : (
+          <h3>権限がありません</h3>
+        )}
       </header>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
